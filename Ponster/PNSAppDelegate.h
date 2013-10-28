7f7759753d984b1533a40c45036f3a7ddaf6b811
alvaro <alvaro@mola.com>
2013-10-28 20:01:44 +0100

    Initial Commit

---
+++ Ponster/PNSAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  PNSAppDelegate.h
+//  Ponster
+//
+//  Created by alvaro on 28/10/13.
+//  Copyright (c) 2013 alvaromb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PNSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Ponster/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ponster
+//
+//  Created by alvaro on 28/10/13.
+//  Copyright (c) 2013 alvaromb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PNSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PNSAppDelegate class]));
+    }
+}
